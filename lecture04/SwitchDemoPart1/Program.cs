645dbd10aa30bd99fef2b5fde5cdf2e4aff76ecf
Anmar PC <aj@anmarjarjees.com>
2025-06-03 15:32:54 -0400

    update

---
+++ lecture04/SwitchDemoPart1/Program.cs
@@ -0,0 +1,101 @@
+﻿// our project name is "ControlFlowAndCollections"
+namespace SwitchDemoPart1
+{
+    internal class Program
+    {
+        // Main method => is the entry point to run our application
+        static void Main(string[] args)
+        {
+            // Switch Statement:
+            // Prompt the user to enter a number between 1 and 5
+            Console.Write("Enter a number from 1 to 5: ");
+
+            // Review: we must always specify the data type for any new variable:
+            string input = Console.ReadLine() ?? ""; // 5 => "5"
+
+            // we are expecting the user to input 1 to 5?
+            // if the user inputs abc!
+            /*
+             * we can ask C# to try parsing the input to an integer number
+             * we are asking C# to try converting the "5" as a string to 5 as an integer
+             * 
+             * int.TryParse():
+             * - int.TryParse("5"): True (it's a numeric value)
+             * - int.TryParse("abc"): False (it's a numeric value)
+             */
+            // bool (boolean) => True or False
+            bool isValidInt = int.TryParse(input, out int number);
+            /*
+             * if the boolean variable "isValidInt" is True => the input is numeric
+             * otherwise the input is not a numeric value
+             */
+            if (isValidInt) // true
+            {
+                // Way1:
+                Console.WriteLine("You entered the number: " + number);
+                // Way2:
+                Console.WriteLine($"You entered the number: {number}");
+                // Way3:
+                Console.WriteLine("You entered the number: {0}", number);
+            }
+            else // false
+            {
+                Console.WriteLine("That wasn't a valid number.");
+                // strop our Main method here
+                // terminate the Main method 
+                // return to our application
+                return; // exit the Main method
+            }
+
+            // if statement:
+            if (number == 1)
+            {
+                Console.WriteLine("You entered ONE!");
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("You entered TWO.");
+            }
+            else if (number == 3)
+            {
+                Console.WriteLine("You entered THREE.");
+            }
+            else if (number == 4)
+            {
+                Console.WriteLine("You entered FOUR.");
+            }
+            else if (number == 5)
+            {
+                Console.WriteLine("You entered FIVE.");
+            }
+            else
+            {
+                Console.WriteLine("You entered a number outside the 1-5 range.");
+            }
+
+            // Switch Statement Syntax:
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("You entered ONE!");
+                    // every case must have break:
+                    break; // to stop the switch statement
+                case 2:
+                    Console.WriteLine("You entered TWO.");
+                    break; // to stop the switch statement
+                case 3:
+                    Console.WriteLine("You entered THREE.");
+                    break;
+                case 4:
+                    Console.WriteLine("You entered FOUR.");
+                    break;
+                case 5:
+                    Console.WriteLine("You entered FIVE.");
+                    break;
+                default:
+                    Console.WriteLine("You entered a number outside the 1-5 range.");
+                    break;
+            } // switch
+        } // end Main
+    } // class
+} // namespace
---
+++ lecture04/SwitchDemoPart2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace SwitchDemoPart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // more examples about Switch Statement
+            double avg = 50;
+            /*
+             * if the avg is greater than or equal to 80 => Well Done
+             * - avg >= 80 => Well Done
+             * - avg >= 70 => Good Job
+             * - else less than 70 => "Never give up! You can try again" 
+             */
+
+            Console.WriteLine("IF-ELSE Example:\n");
+            // NOTE: \n to add a new line
+            if (avg >= 80)
+            {
+                // type cw then tab...
+                // NOTE: No need for "System" as it's already in use (using/imported)
+                System.Console.WriteLine("Well Done!");
+            }
+            else if (avg >= 70)
+            {
+                Console.WriteLine("Good Job!");
+            }
+            else
+            {
+                Console.WriteLine("Never give up! You can try again");
+            }
+
+            Console.WriteLine("\nSWITCH Example:\n");
+            // Switch:
+            switch (avg)
+            {
+                case < 50:
+                    Console.WriteLine($"Your average {avg}; too low.");
+                    break;
+                case > 80.0:
+                    Console.WriteLine($"Your average {avg}; too high.");
+                    break;
+                case > 70:
+                    Console.WriteLine($"Your average {avg}; good.");
+                    break;
+                case >= 60:
+                    Console.WriteLine($"Your average {avg}; Not bad.");
+                    break;
+                case >= 50:
+                    Console.WriteLine($"Your average {avg}; Just Passing.");
+                    break;
+                default:
+                    Console.WriteLine($"Invalid {avg} value!");
+                    break;
+            }
+        }
+    } // class
+}
