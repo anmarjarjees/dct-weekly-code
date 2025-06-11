d6bad61e9ac9bde4dba64d004805e202d761942d
Anmar PC <aj@anmarjarjees.com>
2025-06-11 12:04:38 -0400

    update

---
+++ lecture05/Part1Arrays/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Part1Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arrays (List)");
+
+            int myLuckyNumber = 7;
+            Console.WriteLine(myLuckyNumber); // 7
+
+            // But what if I have 5 lucky numbers?
+            int myLuckyNumber1 = 9;
+            int myLuckyNumber2 = 3;
+            int myLuckyNumber3 = 10;
+            int myLuckyNumber4 = 25;
+            int myLuckyNumber5 = 30;
+
+            // luckyNumbers is an array => []
+            // we declared an array with their values
+            int[] luckyNumbers = { 9, 3, 10, 25, 30 };
+            Console.WriteLine(luckyNumbers); // System.Int32[] => an array of integers
+
+            /*
+             * Array Rules:
+             * ************
+             * - Each element (value) => has its own unique index
+             * - Arrays are 0 based indexed => first element has the index of 0
+             * - The index values can be used to access any individual element
+             * - we use the [] symbol for the index values
+             */
+
+            // Task: output/print the first lucky number => 9
+            Console.WriteLine(luckyNumbers[0]); // 9
+
+            // Task: output/print the second lucky number => 3
+            Console.WriteLine(luckyNumbers[1]); // 3
+
+            // More Examples:
+            // Array of languages:
+            // we declared and initialized their values
+            string[] languages = { "Python", "Java", "C#", "JavaScript" };
+            Console.WriteLine("We are studying " + languages[2]);
+
+            // Array of students:
+            // just declaring a new array of type "string" with 6 elements (students)
+            string[] students = new string[6]; // 6 elements max - highest index = 5
+
+            // Manually assign the names to each element (index) in the array:
+            students[0] = "Alex Chow";
+            students[1] = "Martin Smith";
+            students[2] = "Sam Simpson";
+            students[3] = "Sarah Grandson";
+            students[4] = "Kate Wilson";
+            students[5] = "Elena Chow";
+            // add the 7th element:
+            // students[6] = "James Dean"; // Error
+            /*
+             * System.IndexOutOfRangeException
+             * Message=Index was outside the bounds of the array.
+             * 
+             * Exception => an error happening in the runtime
+             */
+
+            // 2D: => it's an array inside another array FYI :-)
+            /*
+             * Countries And Their Capital Cities:
+             * Canada => Ottawa
+             * USA => Washington
+             * UK => London
+             * Japan => Tokyo
+             */
+            string[,] countriesAndCapitals =
+            {
+                // every element is an array that contains 2 elements: Country, Capital
+                // element of index 0 => { "Canada", "Ottawa"}
+                { "Canada", "Ottawa"}, // index 0 => Canada and index 1 => Ottawa
+                // element of index 1 => { "USA", "Washington"},
+                { "USA", "Washington"},
+                { "UK", "London" },
+                { "Japan", "Tokyo" }
+            };
+
+            // Task: output "The capital of Canada is Ottawa"
+            Console.WriteLine("The capital of Canada is " + countriesAndCapitals[0, 1]);
+
+        } // Main
+    } // class
+} // namespace
---
+++ lecture05/Part2Loops/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Part2Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loops");
+
+            // iterate 5 times: 1 to 5
+            // for loop:
+            // i++ => just adding 1 
+            // condition to keep iterating since i <= 5
+            Console.WriteLine("For Loop:");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("For Loop Counter: " + i);
+            }
+
+
+            // while loop:
+            // \n => a new line:
+            Console.WriteLine("\nWhile Loop:");
+            int j = 1;
+            while (j <= 5)
+            {
+                Console.WriteLine("While Loop Counter: " + j);
+                j++; // we do need to increment the loop counter
+                /*
+                 * When j became 6 => the loop will stop => the condition is false
+                 */
+            }
+
+            // do-while loop:
+            Console.WriteLine("\nDo-While Loop:");
+            int k = 1;
+            do
+            {
+                Console.WriteLine("Loop Counter: " + k);
+                k++;
+            }
+            while (k <= 5);
+
+            // Finally: foreach:
+            // Used with arrays, and lists
+            Console.WriteLine("\nForeach Loop:");
+            string[] printingColors = { "Cyan", "Magenta", "Yellow", "Black" };
+            foreach (string color in printingColors)
+            {
+                Console.WriteLine("Color: " + color);
+            }
+
+            Console.WriteLine("\nFor Loop with Array:");
+            // we can still use for, while, do-while with arrays:
+            // the loop counter "i" will represent the index value of each element (color)
+            // Arrays are 0 based index!
+            for (int i = 0; i < printingColors.Length; i++)
+            {
+                Console.WriteLine(printingColors[i]);
+            }
+        } // Main()
+    } // class
+} // namespace
+
