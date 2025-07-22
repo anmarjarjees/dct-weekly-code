2b46d642d4b320604f1579af86e94f42ba8439f4
Anmar PC <aj@anmarjarjees.com>
2025-07-22 15:27:03 -0400

    update

---
+++ lecture08/OOPBasics/Person.cs
@@ -0,0 +1,110 @@
+﻿// A PIE
+namespace OOPBasics
+{
+    // This "class" will be used as Blueprint 
+    /*
+     * Class can have two members:
+     * - Properties
+     * - Methods
+     */
+    internal class Person
+    {
+        /*
+         * Access Modifiers (Visibility Modifiers):
+         * - Private => can only used inside the class itself
+         * - Public => can be used inside/outside the class
+         * - Protected => for later
+         * - Internal => can be used within the same assembly 
+         */
+        // no Main() method in this class:
+        // Class Fields:
+        private string _name, _email, _address;
+        private int _age;
+        /*
+         * C# Naming Convention:
+         * any private field should start with "_"
+         */
+
+        // create public properties to access the private fields:
+        // Properties can have: set and get
+        // We create "public" properties to access "private" fields
+
+        // template => AccessModifier ReturnedDataType FiledName { }
+        // Property for the "_name" field:
+        public string Name
+        {
+            // getting the person name:
+            get { return _name; }
+            // setting a new value to person name:
+            set { _name = value; }
+        }
+
+        // Property for the "email" field:
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        // Property for the "age" field:
+        /*
+         * we don't have 0 age or negative age!!!
+         */
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid age! Must be non-negative value.");
+                }
+            }
+        }
+
+        /*
+         * NOTE:
+         * *****
+         * if we need a custom constructor with x number of parameters
+         * we have to create it by ourselves, we can create as much as we need!
+         * 
+         * we need this custom constructor to initialize the private fields:
+         * - name
+         * - email
+         * - age
+         * 
+         * VERY IMPORTANT NOTE:
+         * ********************
+         * When we create a custom constructor that accept parameter(s),
+         * This will destroy the default no parameter constructor
+         * that we receive by default when we create a new class!
+         * 
+         * Solution:
+         * if you still need the default constructor,
+         * we need to explicitly create this default constructor again!
+         */
+        public Person()
+        {
+            // no need for any code!
+        }
+
+        public Person(string name, string email, int age)
+        {
+            // x = x ? In programming Ambiguous operation!
+            this._name = name;
+            this._email = email;
+            this._age = age;
+        }
+
+        // Methods: same as with your second assignment :-)
+        // Print the person information:
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine($"Name: {_name}\nAge: {_age}\nEmail: {_email}");
+        }
+    } // class
+} // namespace
---
+++ lecture08/OOPBasics/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace OOPBasics
+{
+    /*
+     * Inside the class "Program":
+     * we can call/run/execute the class "Person"
+     * but we cannot access any private field/member
+     */ 
+    internal class Program
+    {
+        // any C# application must have a class with the Main()
+        // Main() is the main entry point to run our application
+        static void Main(string[] args)
+        {
+            Console.WriteLine("** Call and Run our Class 'Person' **");
+            // creating our first person (object):
+            // Name: Alex Chow - Age: 20 - Email: AlexChow@example.com
+            // person1 is a variable => every variable must have a data type
+            // person1 is of type "Person"
+            /*
+             * new Person() => calling a special method "Constructor"
+             * - Every class is shipped with "Default Constructor"
+             * - Default Constructor, free one :-):
+             *   > A constructor that has NO parameters
+             */
+            Person person1 = new Person();
+
+            // we call the public properties to set values for person1
+            // we are calling the "setters" to set values
+            // Property Template: objectName.PropertyName = Any Value
+            person1.Name = "Alex Chow";
+            person1.Age = 20;
+            // person1.Email = string.Empty; // has no email (no string)
+            person1.Email = "AlexChow@example.com";
+
+            /*
+             * - Q) Can't we create a new person and passing its values
+             * at the same line when we use the "new" keyword
+             * to call the constructor?
+             * - Yes :-)
+             * - How?
+             * we need to add/create custom constructor(s) with parameters!
+             * we can create another constructor with 3 parameters,
+             * to accept these 3 arguments:
+             * - Martin Smith
+             * - m.smith@example.com
+             * - 23
+             */
+
+            /*
+             * Example:
+             * we want to create a new person:
+             * - Name: Martin Smith
+             * - Email: m.smith@example.com
+             * - age: 23
+             */
+
+            /*
+             * Remember: 
+             * We can have only one default constructor (No parameters) for free :-)
+             * 
+             * But below, we are calling a "Constructor" with parameters!
+             * Do we have this type of constructors?
+             * - if No => we have to created by ourselves
+             */ 
+            Person person2 = new Person("Martin Smith", "m.smith@example.com", 23);
+            //Person person2 = new Person();
+            // adding the value
+
+            // calling the methods for our objects:
+            // person1 and person2
+            // Method Template: objectName.MethodName() 
+            person1.PrintPersonInfo();
+            Console.WriteLine("\n");
+            person2.PrintPersonInfo();
+        } // Main
+    } // class
+
+    /*
+     * - Can we add here our custom class(es)?
+     * - Yes, but not frequently used (not recommended practice!) 
+     */ 
+} // namespace
